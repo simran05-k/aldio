9de4c230ac54994da2ef1fd2162c71803ce7628e
Chrishan <real.simran05@gmail.com>
2020-06-16 11:18:52 +0530

    first push

---
+++ header.php
@@ -0,0 +1,131 @@
+<html>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!-- Custom Css Link -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/2.0.46/css/materialdesignicons.min.css">
+
+    <title>Home </title>
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row py-3">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="#">
+                        <img src="./images/brand-logo.svg" class="img-fluid" alt="brand-logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item dropdown active">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Product<span class="sr-only">(current)</span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Solution
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pricing
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Snapbyte
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Resources
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                            <li class="nav-item dropdown pr-3">
+                                <a class="nav-link dropdown-toggle text-uppercase" href="#" id="navbarDropdownMenuLink"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    En <span class="sr-only">(current)</span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+
+                            <li class="nav-item dropdown pr-3">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    BigCommand <span class="sr-only">(current)</span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+
+                            <li class="nav-item pr-3">
+                                <a class="nav-link" href="#">
+                                    Sign in
+                                </a>
+                            </li>
+                        </ul>
+
+                        <button class="btn btn-success my-2 my-sm-0" type="submit">Sign Up Free</button>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php include 'header.php';?>
+
+    <!-- Video Section -->
+    <section>
+        <div class="container-fluid">
+            <div class="row d-flex align-items-center align-content-center">
+                <div class="col-md-6">
+                    <div>
+                        <h1>World’s<br> Lightest, Fastest <br>& Most Secure <br>
+                            <span class="cat-text">4K Video Player</span>
+                        </h1>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="./images/video-player.png" class="img-fluid" alt="video player">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Video Section END -->
+
+    <!-- An Interactive HTML5 Player Section -->
+
+    <section class="my-5">
+        <div class="container-fluid">
+            <div class="row justify-content-center text-center">
+                <div class="col-md-8">
+                    <h2>An Interactive HTML5 Player</h2>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <img src="./images/html-player.png" class="img-fluid" alt="HTML Player">
+                </div>
+                <div class="col-md-6">
+                    <div>
+                        <ul>
+                            <li>Prequalify, convert and capture leads </li>
+                            <li>Sell products & schedule appointments</li>
+                            <li>Promote your other videos</li>
+                            <li>Insert dynamic ads on your videos </li>
+                            <li> Redirect to websites and links </li>
+                            <li>Loop videos</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- An Interactive HTML5 Player Section END-->
+
+
+    <section>
+        <div class="container-fluid">
+            <div class="row my-5">
+                <div class="col-md-4">
+                    <div>
+                        <h2>You Control <br>the Controls</h2>
+                        <div class="text-right pr-sm-5 mt-sm-5"><img src="./images/group-1.png" class="img-fluid"></div>
+                    </div>
+                </div>
+                <div class="col-md-8 cat-image">
+                   <img src="./images/blob-img.png" class="blob-img">
+                    <img src="./images/cat-img.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include 'footer.php';?>
+
+
+
+
+
+
+
+
+
+
+
